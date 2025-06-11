a340a5d515763ee33323e4ee65d24f0078cb72d6
Srivaths <srivaths.iyer@gmail.com>
2025-06-11 22:47:29 +0530

    Minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/digital_portfolio/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  }
 })
